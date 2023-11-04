0d79016e73faf07e4ff47a6c079179928be6f6fa
Simon Boylan <siboy73@gmail.com>
2023-11-04 18:52:28 +0000

    doc: update styling to github page css

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,38 +1,38 @@
-function userInformationHTML () {
+function userInformationHTML(user) {
   return `
-  <h2>${user.name}
-    <span class="small-name">
-      (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
-    </span>
-  </h2>
-  <div class="gh-content">
-    <div class="gh-avatar">
-      <a href="${user.html_url}" target="_blank">
-        <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
-      </a>
-    </div>
-      <p>Followers: ${user.follows} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-  </div>`;
+      <h2>${user.name}
+          <span class="small-name">
+              (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+          </span>
+      </h2>
+      <div class="gh-content">
+          <div class="gh-avatar">
+              <a href="${user.html_url}" target="_blank">
+                  <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+              </a>
+          </div>
+          <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+      </div>`;
 }
 
 function repoInformationHTML(repos) {
   if (repos.length == 0) {
-    return `<div class="clearfix repo-list">No repos!</div>`;
+      return `<div class="clearfix repo-list">No repos!</div>`;
   }
 
   var listItemsHTML = repos.map(function(repo) {
-    return `<li>
-              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-            </li>`;
+      return `<li>
+                  <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+              </li>`;
   });
 
   return `<div class="clearfix repo-list">
-            <p>
-              <strong>Repo List:</strong>
-            </p>
-            <ul>
-              ${listItemHTML.join("\n")}
-            </ul>
+              <p>
+                  <strong>Repo List:</strong>
+              </p>
+              <ul>
+                  ${listItemsHTML.join("\n")}
+              </ul>
           </div>`;
 }
 
@@ -40,33 +40,33 @@ function fetchGitHubInformation(event) {
 
   var username = $("#gh-username").val();
   if (!username) {
-    $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
-    return;
+      $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+      return;
   }
 
   $("#gh-user-data").html(
-    `<div id="loader">
-      <img src="assets/css/loader1.gif" alt="loading..."/>
+      `<div id="loader">
+          <img src="assets/css/loader.gif" alt="loading..." />
       </div>`);
 
   $.when(
-    $.getJSON(`https://api.github.com/users/${username}`),
-    $.getJSON(`https://api.github.com/users/${username}/repos`)
+      $.getJSON(`https://api.github.com/users/${username}`),
+      $.getJSON(`https://api.github.com/users/${username}/repos`)
   ).then(
-    function(firstResponse, secondResponse) {
-      var userData = firstResponse[0];
-      var repoData = secondResponse[0];
-      $("#gh-user-data").html(userInformationHTML(userData));
-      $("#gh-repo-data").html(repoInformationHTML(repoData));
-    }, 
-    function(errorResponse) {
-      if (errorResponse.status === 404) {
-        $("#gh-user-data").html(
-          `<h2>No info found for user ${username}</h2>`);
-      } else {
-        console.log(errorResponse);
-        $("gh-user-data").html(
-          `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-        }
-    });
+      function(firstResponse, secondResponse) {
+          var userData = firstResponse[0];
+          var repoData = secondResponse[0];
+          $("#gh-user-data").html(userInformationHTML(userData));
+          $("#gh-repo-data").html(repoInformationHTML(repoData));
+      },
+      function(errorResponse) {
+          if (errorResponse.status === 404) {
+              $("#gh-user-data").html(
+                  `<h2>No info found for user ${username}</h2>`);
+          } else {
+              console.log(errorResponse);
+              $("#gh-user-data").html(
+                  `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+          }
+      });
 }
